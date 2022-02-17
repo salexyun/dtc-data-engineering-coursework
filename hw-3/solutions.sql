84036a68f7d61db67b40c5aba64498814c37ff0f
Alex Yun <s.alexyun@gmail.com>
2022-02-16 21:05:18 -0500

    dbt setup

--- hw-3/solutions.sql
+++ hw-3/solutions.sql
@@ -14,4 +14,4 @@ SELECT * FROM dtc-de-course-339303.nyc_taxi.fhv_tripdata;
 
 SELECT COUNT(*) FROM `dtc-de-course-339303.nyc_taxi.fhv_tripdata_partitioned`
 WHERE DATE(dropoff_datetime) BETWEEN '2019-01-01' AND '2019-03-31'
-AND (dispatching_base_num='B00987' OR dispatching_base_num='B02060' OR dispatching_base_num='B02279');
+AND dispatching_base_num IN ('B00987', 'B02060', 'B02279');
